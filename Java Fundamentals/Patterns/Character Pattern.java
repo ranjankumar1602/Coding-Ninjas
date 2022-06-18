bac6fa567fcfb1cd9317f130f1dbaf0f88664567
Ranjan Kumar <91800094+ranjankumar1602@users.noreply.github.com>
2022-06-18 17:08:00 +0530

    Create Character Pattern.java

---
+++ Java Fundamentals/Patterns/Character Pattern.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Solution {
+
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int i=1;
+        while(i<=N){
+            int j=1;
+            int p=i;
+            while(j<=i){
+                char jth=(char)('A'+p-1);
+                System.out.print(jth);
+                p++;
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+
+		
+	}
+
+}
