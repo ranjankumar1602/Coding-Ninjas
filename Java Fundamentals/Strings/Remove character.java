fd2da80236be35b09d78c78a36ff3176121f4603
Ranjan Kumar <91800094+ranjankumar1602@users.noreply.github.com>
2022-06-19 23:36:49 +0530

    Create Remove character.java

---
+++ Java Fundamentals/Strings/Remove character.java
@@ -0,0 +1,15 @@
+
+public class Solution {
+
+	public static String removeAllOccurrencesOfChar(String str, char ch) {
+		// Your code goes here
+        String str1="";
+        for(int i=0; i<str.length(); i++) {
+            if(str.charAt(i)!=ch) {
+                str1+=str.charAt(i);
+            }
+        }
+        return str1;
+	}
+
+}
