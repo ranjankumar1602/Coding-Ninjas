9cc544a21a05a4b568d402c53bb0e24af9486f4d
Ranjan Kumar <91800094+ranjankumar1602@users.noreply.github.com>
2022-06-24 01:00:30 +0530

    Create Check Array Rotation.java

---
+++ Data Structure in Java/Lec 5 Time Complexity/Check Array Rotation.java
@@ -0,0 +1,21 @@
+public class Solution {
+
+    public static int arrayRotateCheck(int[] arr){
+    	//Your code goes here
+        int min_index=Integer.MAX_VALUE;
+        int min_element=Integer.MAX_VALUE;
+        int len=arr.length;
+        
+        if(len==0) {
+            return 0;
+        }
+        for(int i=0;i<len;i++) {
+            if(arr[i]<min_element) {
+                min_element=arr[i];
+                min_index=i;
+            }
+        }
+        return min_index;
+    }
+
+}
