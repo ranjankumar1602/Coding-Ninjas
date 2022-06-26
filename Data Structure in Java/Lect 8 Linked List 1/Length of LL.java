6deca9c667a08aa0b0b3c0f6cac84faa1658b13b
Ranjan Kumar <91800094+ranjankumar1602@users.noreply.github.com>
2022-06-26 23:30:44 +0530

    Create Length of LL.java

---
+++ Data Structure in Java/Lect 8 Linked List 1/Length of LL.java
@@ -0,0 +1,12 @@
+public class Solution {
+
+	public static int length(LinkedListNode<Integer> head){
+		//Your code goes here
+        int count=0;
+        while(head!=null) {
+            count++;
+            head=head.next;
+        }
+        return count;
+	}
+}
