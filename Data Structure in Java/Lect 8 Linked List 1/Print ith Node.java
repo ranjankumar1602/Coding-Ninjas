ffaca15d51ea21d3055f21976aaee68e77068b53
Ranjan Kumar <91800094+ranjankumar1602@users.noreply.github.com>
2022-06-26 23:56:22 +0530

    Create Print ith Node.java

---
+++ Data Structure in Java/Lect 8 Linked List 1/Print ith Node.java
@@ -0,0 +1,35 @@
+/*
+
+ 	Following is the Node class already written for the Linked List
+
+	class LinkedListNode<T> {
+		T data;
+		LinkedListNode<T> next;
+
+		public Node(T data) {
+			this.data = data;
+		}
+	}
+
+*/
+
+public class Solution {
+
+	public static void printIthNode(LinkedListNode<Integer> head, int i){
+		//Your code goes here
+        int count=0;
+        if(head==null) {
+            System.out.println();
+            return;
+        }
+        LinkedListNode<Integer> temp= head;
+        while(temp.next!=null) {
+            temp=temp.next;
+            count++;
+            if(count==i) {
+                System.out.println(temp.data);
+            }
+        }
+        
+	}
+}
