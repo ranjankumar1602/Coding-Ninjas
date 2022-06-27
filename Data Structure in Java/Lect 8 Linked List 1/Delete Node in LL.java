55cf769a1bc4d7c826dc7bccf162b0a239e64a53
Ranjan Kumar <91800094+ranjankumar1602@users.noreply.github.com>
2022-06-27 23:37:40 +0530

    Create Delete Node in LL.java

---
+++ Data Structure in Java/Lect 8 Linked List 1/Delete Node in LL.java
@@ -0,0 +1,40 @@
+/****************************************************************
+
+	Following is the Node class already written for the Linked List
+
+	class LinkedListNode<T> {
+    	T data;
+    	LinkedListNode<T> next;
+    
+    	public LinkedListNode(T data) {
+        	this.data = data;
+    	}
+	}
+
+*****************************************************************/
+
+public class Solution {
+	public static LinkedListNode<Integer> deleteNode(LinkedListNode<Integer> head, int pos) {
+		// Write your code here.
+        if(head==null) {
+            return head;
+        }
+        if(pos==0) {
+            return head.next;
+        }
+        int count=0;
+        LinkedListNode<Integer> temp=head;
+        while(temp!=null && count<pos-1) {
+            temp=temp.next;
+            count++;
+        }
+        if(temp==null) {
+            return head;
+        }
+        if(temp.next!=null) {
+            temp.next=temp.next.next;
+        }
+        return head;
+        
+	}
+}
