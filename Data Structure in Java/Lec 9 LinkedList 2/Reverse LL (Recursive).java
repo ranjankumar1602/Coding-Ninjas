211e0749a5b32869a5f444d7dd2914a0b6e43266
Ranjan Kumar <91800094+ranjankumar1602@users.noreply.github.com>
2022-07-02 00:18:28 +0530

    Create Reverse LL (Recursive).java

---
+++ Data Structure in Java/Lec 9 LinkedList 2/Reverse LL (Recursive).java
@@ -0,0 +1,35 @@
+/*
+
+    Following is the Node class already written for the Linked List
+
+    class LinkedListNode<T> {
+        T data;
+        LinkedListNode<T> next;
+    
+        public LinkedListNode(T data) {
+            this.data = data;
+        }
+    }
+
+*/
+
+public class Solution {
+
+	public static LinkedListNode<Integer> reverseLinkedListRec(LinkedListNode<Integer> head) {
+		//Your code goes here
+        if(head==null || head.next==null) {
+            return head;
+        }
+        
+        LinkedListNode<Integer> newhead=reverseLinkedListRec(head.next);
+        LinkedListNode<Integer> temp=newhead;
+        while(temp.next != null) {
+            temp=temp.next;
+        }
+        temp.next=head;
+        head.next=null;
+        
+        return newhead;
+	}
+
+}
