a7db3bb292f10ec7d7785e5a704933dce643555c
Ranjan Kumar <91800094+ranjankumar1602@users.noreply.github.com>
2022-07-08 00:46:25 +0530

    Create Minimum bracket Reversal.java

---
+++ Data Structure in Java/Lec 10 Stacks/Minimum bracket Reversal.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+public class Solution {
+
+	public static int countBracketReversals(String input) {
+		//Your code goes here
+        if(input.length()%2!=0) {
+            return -1;
+        }
+        Stack<Character> stack= new Stack<>();
+        int count=0;
+        for (int i=0; i<input.length(); i++) {
+            char c=input.charAt(i);
+            if (c=='{') {
+                
+                stack.push(c);
+                
+            } else {
+                
+                if (stack.isEmpty()) {
+                    stack.push(c);
+                } else if (stack.peek()=='}') {
+                    stack.push(c);
+                } else if (stack.peek()=='{') {
+                    stack.pop();
+                }
+            }
+        }
+        
+        while(!stack.isEmpty()) {
+            
+            char c1=stack.pop();
+            char c2=stack.pop();
+            
+            if (c1==c2) {
+                count=count+1;
+            } else {
+                count=count+2;
+            }
+        }
+        return count;
+	}
+
+}
