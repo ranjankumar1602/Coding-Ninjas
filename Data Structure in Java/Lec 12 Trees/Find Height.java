9faa67cfbe7a44836e321c27e8991323501486a4
Ranjan Kumar <91800094+ranjankumar1602@users.noreply.github.com>
2022-07-22 23:04:01 +0530

    Create Find Height.java

---
+++ Data Structure in Java/Lec 12 Trees/Find Height.java
@@ -0,0 +1,38 @@
+public class Solution {
+
+	/*	TreeNode structure 
+	 * 
+	 * class TreeNode<T> {
+		T data;
+		ArrayList<TreeNode<T>> children;
+
+		TreeNode(T data){
+			this.data = data;
+			children = new ArrayList<TreeNode<T>>();
+		}
+	}*/
+
+	public static int getHeight(TreeNode<Integer> root){
+		/* Your class should be named Solution
+		 * Don't write main().
+		 * Don't read input, it is passed as function argument.
+		 * Return output and don't print it.
+		 * Taking input and printing output is handled automatically.
+		 */
+        if(root==null) {
+            return 0;
+        }
+        if(root.children.size()==0) {
+            return 1;
+        }
+        int temp=0,height=0;
+        for(int i=0;i<root.children.size();i++) {
+            temp=getHeight(root.children.get(i));
+            if(temp>height) {
+                height=temp;
+            }
+        }
+        return height+1;
+	}
+
+}
