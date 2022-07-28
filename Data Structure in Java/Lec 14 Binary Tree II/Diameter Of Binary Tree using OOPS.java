b6cbae6e14b26f6669fb7fb799d3dee878906c0d
Ranjan Kumar <91800094+ranjankumar1602@users.noreply.github.com>
2022-07-29 00:07:00 +0530

    Create Diameter Of Binary Tree using OOPS.java

---
+++ Data Structure in Java/Lec 14 Binary Tree II/Diameter Of Binary Tree using OOPS.java
@@ -0,0 +1,32 @@
+class Pair {
+    int diameter;
+    int height;
+    public Pair(int diameter, int height) {
+        this.diameter=diameter;
+        this.height=height;
+    }
+}
+public class Solution {
+    private static Pair diameterHelper(BinaryTreeNode<Integer> root) {
+        if(root == null) {
+            Pair pair=new Pair(0,0);
+            return pair;
+        }
+        Pair leftPair=diameterHelper(root.left);
+        Pair rightPair=diameterHelper(root.right);
+        
+        int leftDiameter=leftPair.diameter;
+        int rightDiameter=rightPair.diameter;
+        
+        int diatance=leftPair.height +rightPair.height +1;
+        int diameter=Math.max(leftDiameter,Math.max(rightDiameter,distance));
+        int height=Math.max(leftPair.height,rightPair.height)+1;
+        
+        return (new Pair(diameter,height));
+    }
+    
+    public static int diameterOfBinaryTree(BinaryTreeNode<Integer> root) {
+        Pair pair=diameterHelper(root);
+        return pair.diameter;
+    }
+}
