5f8ad3ccd3e6421609503b6a9cbe664274430b39
Ranjan Kumar <91800094+ranjankumar1602@users.noreply.github.com>
2022-07-30 15:10:27 +0530

    Create Path Sum Root to Leaf.java

---
+++ Data Structure in Java/Lec 14 Binary Tree II/Path Sum Root to Leaf.java
@@ -0,0 +1,42 @@
+/*
+	
+	Following is the structure used to represent the Binary Tree Node
+
+	class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+
+		public BinaryTreeNode(T data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
+*/
+
+public class Solution {
+    
+    public static void rootToLeafPathsSumToK(BinaryTreeNode<Integer> root, int k, String s) {
+        if(root==null) {
+            return;
+        }
+        int rootData=root.data;
+        s = s + rootData +" ";
+        if(rootData == k && root.left == null && root.right == null) {
+            System.out.println(s);
+            return;
+        }
+        rootToLeafPathsSumToK(root.left, k - rootData, s);
+        rootToLeafPathsSumToK(root.right, k - rootData, s);
+    }
+
+	public static void rootToLeafPathsSumToK(BinaryTreeNode<Integer> root, int k) {
+		//Your code goes here
+        String s="";
+        rootToLeafPathsSumToK(root,k,s);
+        
+	}
+
+}
