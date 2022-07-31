6e84b0d82c1be04a3b82f832ffe072a6dcbfa9fd
Ranjan Kumar <91800094+ranjankumar1602@users.noreply.github.com>
2022-07-31 10:53:14 +0530

    Create Print nodes at distance k from node.java

---
+++ Data Structure in Java/Lec 14 Binary Tree II/Print nodes at distance k from node.java
@@ -0,0 +1,72 @@
+
+/*
+	
+	Following is the structure used to represent the Binary Tree Node
+
+	class BinaryTreeNode<T> {
+		T data;
+		BinaryTreeNode<T> left;
+		BinaryTreeNode<T> right;
+
+		public BinaryTreeNode(T data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
+	}
+
+*/
+
+public class Solution {
+
+	public static void nodesAtDistanceK(BinaryTreeNode<Integer> root, int node, int k) {
+	    //Your code goes here
+        printkdistanceNode(root,node,k);
+	}
+    
+    public static int printkdistanceNode(BinaryTreeNode<Integer> root, int node, int k){
+        if (root == null){
+            return -1;
+        }    
+        if (root.data == node){
+            printkdistanceNodeDown(root, k);
+            return 0;
+        }
+        int dl = printkdistanceNode(root.left, node, k);
+        if (dl != -1){
+            if (dl + 1 == k){
+                System.out.print(root.data);
+                System.out.println("");
+            }
+            else{
+                printkdistanceNodeDown(root.right, k - dl - 2);
+            }   
+            return 1 + dl;
+        }
+        int dr = printkdistanceNode(root.right, node, k);
+        if (dr != -1){
+            if (dr + 1 == k){
+                System.out.print(root.data);
+                System.out.println("");
+            }
+            else{
+                printkdistanceNodeDown(root.left, k - dr - 2);
+            }   
+            return 1 + dr;
+        }
+        return -1;
+    }
+    
+    public static void printkdistanceNodeDown(BinaryTreeNode<Integer> root, int k){
+        if (root == null || k < 0){
+            return;
+        }
+        if (k == 0){
+            System.out.print(root.data);
+            System.out.println("");
+            return;
+        }
+        printkdistanceNodeDown(root.left, k - 1);
+        printkdistanceNodeDown(root.right, k - 1);
+    }
+}
